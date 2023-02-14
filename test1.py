fbdadd8b8484a8f510306d7e1eda9f7dc72ded54
mitatskni <mitatskni@gmail.com>
2023-02-14 12:56:57 +0100

    мой первый коммит

--- test1.py
+++ test1.py
@@ -1,1 +1,10 @@
-print('haalo')
+a, b = map(int, input().split())
+
+matrix = [['.'] * b for _ in range(a)]
+
+for i in range(a):
+    for j in range(b):
+        if (i + j) % 2 == 1:
+            matrix[i][j] = '*'
+for i in matrix:
+    print(*i)

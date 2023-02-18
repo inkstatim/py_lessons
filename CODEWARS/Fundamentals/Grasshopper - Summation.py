e11f518cffbfdd2b162dfdc491ff676fe3808329
mitatskni <mitatskni@gmail.com>
2023-02-18 20:26:22 +0100

    сумма чисел

---
+++ CODEWARS/Fundamentals/Grasshopper - Summation.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+
+"""Write a program that finds the summation of every number from 1 to num.
+The number will always be a positive integer greater than 0."""
+
+
+def summation(num):
+    suma = 0
+    for i in range(1, num+1):
+        suma+=i
+    return suma
+
+
+

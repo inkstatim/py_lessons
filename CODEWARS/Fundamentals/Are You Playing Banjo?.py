fe8667387d114cccccaa912cc01a82851e62a5a2
mitatskni <mitatskni@gmail.com>
2023-02-18 11:20:41 +0100

    moved

---
+++ CODEWARS/Fundamentals/Are You Playing Banjo?.py
@@ -0,0 +1,4 @@
+#https://www.codewars.com/kata/53af2b8861023f1d88000832/train/python
+
+def are_you_playing_banjo(name):
+    return f"{name} plays banjo" if name.lower()[0] =='r' else f"{name} does not play banjo"

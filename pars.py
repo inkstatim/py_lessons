14bb5b422a0a1a37b20f594180507a340b642566
mitatskni <mitatskni@gmail.com>
2023-02-19 16:56:10 +0100

    Codewars задачки

---
+++ pars.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Получаем HTML-код страницы
+response = requests.get('https://de.wiktionary.org/wiki/habt ')
+html = response.text
+
+# Используем BeautifulSoup для парсинга HTML-кода
+soup = BeautifulSoup(html, 'html.parser')
+
+# Находим нужный элемент на странице
+element = soup.find('div', {'class': 'some-class'})
+
+# Проверяем, что элемент найден
+if element is not None:
+    # Получаем текстовое содержимое элемента
+    data = element.text
+    # Сохраняем данные в файл
+    with open('data.txt', 'w') as file:
+        file.write(data)
+else:
+    print('Элемент не найден на странице')
